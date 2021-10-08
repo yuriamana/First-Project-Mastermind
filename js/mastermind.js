ab9c121a5ce0daf473b40dc2c6d85850f23027af
Yuki Kato <katoyuki0113@gmail.com>
2021-10-08 09:34:53 +0200

    final

--- js/mastermind.js
+++ js/mastermind.js
@@ -3,7 +3,10 @@ const colorSelector = document.querySelectorAll(".circle");
 const selectedColor = document.querySelector("#selected-colors-container");
 const hintSelector = document.querySelector(".hint");
 const btnStart = document.getElementById("btn-start");
-const hintArray = [];
+const btnReset = document.getElementById("btn-reset");
+let imageWhenYouWin = document.getElementById("won");
+let imageWhenYouLose = document.getElementById("loser");
+//const hintArray = [];
 let round = 1;
 let count = 0;
 var solution;
@@ -15,18 +18,20 @@ function shuffle(arr) {
     let temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
-   
   }
   return arr;
 }
 
 function startGame() {
   solution = shuffle(colors);
-  console.log(solution)
+  console.log(solution);
   launchGameLogic();
 }
 
 btnStart.onclick = startGame;
+btnReset.addEventListener("click", () => {
+  console.log("btnReset");
+});
 
 function launchGameLogic() {
   colorSelector.forEach((btn) => {
@@ -34,8 +39,9 @@ function launchGameLogic() {
 
     btn.addEventListener("click", () => {
       if (count === 0) {
+        //console.log("ici");
         newLine = createNewRoundLine();
-        // displayHints();
+        //console.log(newLine);
       }
       if (count < colors.length) {
         count++;
@@ -44,11 +50,13 @@ function launchGameLogic() {
 
       if (count >= colors.length) {
         if (round < 8) {
-            count = 0;
-            displayHints();
-            round++;
+          count = 0;
+          displayHints();
+          win();
+          round++;
         } else {
           alert("GAME OVER");
+          imageWhenYouLose.style.display = "block";
         }
       }
     });
@@ -56,58 +64,73 @@ function launchGameLogic() {
 }
 
 function displayHints() {
-  const guessedColor = document.querySelectorAll(`#round-${round} .selected-color`);
-  console.log(guessedColor);
+  const guessedColor = document.querySelectorAll(
+    `#round-${round} .selected-color`
+  );
+
   let hints = [];
   let duplicateCheck = [];
-    //console.log(guessedColor);
+
   guessedColor.forEach((color, index) => {
-      color = color.classList[1];
+    color = color.classList[1];
     if (color === solution[index]) {
-        hints.push("full");
-        duplicateCheck.push(color);
-        //console.log('=', color, solution[index]);
+      hints.push("full");
+      duplicateCheck.push(color);
     }
   });
 
-    guessedColor.forEach((color, index) => {
-        color = color.classList[1];
-     if(!duplicateCheck.includes(color) && solution.includes(color)) {
-         hints.push("half");
-         console.log(color);
-     }
+  guessedColor.forEach((color, index) => {
+    color = color.classList[1];
+    if (!duplicateCheck.includes(color) && solution.includes(color)) {
+      hints.push("half");
+      //console.log(color);
+    }
   });
-   //console.log(solution);
-   console.log(hints);
+
+  const hintWrapper = document.createElement("div");
+  hintWrapper.classList.add("hint-wrapper");
+  const target = document.getElementById("round-" + round);
 
   for (const hint of hints) {
-      const el = document.createElement("div");
-      if (hint === "full") {
-          el.classList.add("full");
-      } else {
-          el.classList.add("half");
-      }
-      selectedColor.appendChild(el);
+    const el = document.createElement("div");
+    if (hint === "full") {
+      el.classList.add("full");
+    } else {
+      el.classList.add("half");
+    }
+    hintWrapper.appendChild(el);
   }
-};
 
+  target.appendChild(hintWrapper);
+}
 
 function createNewRoundLine() {
   const div = document.createElement("div");
   div.id = "round-" + round;
   div.classList.add("round-line");
-  selectedColor.append(div);
+  const selection = document.createElement("div");
+  selection.classList.add("selection");
+  div.appendChild(selection);
   return div;
 }
 
 function createColorCircle(color, line) {
-  //console.log(color)
-  
+  //console.log(line)
+  const selection = line.querySelector(".selection");
+  //console.log("selection ?", selection);
   const div = document.createElement("div");
   div.classList.add("selected-color");
   div.classList.add(color);
-  line.append(div);
+  selection.appendChild(div);
+  selectedColor.appendChild(line);
 }
 
-
-
+function win() {
+  const target = document.getElementById("round-" + round);
+  const won = target.querySelectorAll(".full");
+  let checkFull = won.length === 4 ? true : false;
+  if (checkFull) {
+    alert("YOU WON!");
+    imageWhenYouWin.style.display = "block";
+  }
+} 
